b43d55a92bf0aef22e0866e66d0bd64cb472e358
kloba1004 <ante.bebic@gmail.com>
2020-10-23 19:04:40 +0200

    Finished a workflow for displaying new projects and todos on the screen after creation.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import Editor from './editor.js';
 import PushToDisplay from './push-to-display.js';
 import PullFromDisplay from './pull-from-display.js';
 
-const form = document.querySelector('.todo-form');
+const forms = document.querySelectorAll('form');
 
-form.addEventListener('submit', (e) =>{
-    const inputs = PullFromDisplay.download();
-    const toDo = Editor.createItem(ToDo, ...inputs);
-    PushToDisplay.print(toDo);
+forms.forEach(form => form.addEventListener('submit', (e) =>{
+    const itemType = e.target.className;
+    const inputs = PullFromDisplay.download(`.${itemType}-input`);
+    if (itemType === 'Project') {
+        var item = Editor.createItem(Project, ...inputs);
+    } else {
+        var item = Editor.createItem(ToDo, ...inputs);
+    }
+    PushToDisplay.print(item);
+
     e.preventDefault();
     e.target.reset();
-});
+}));
--- src/pull-from-display.js
+++ src/pull-from-display.js
@@ -1,6 +1,6 @@
 class PullFromDisplay {
-    static download() {
-        const inputs = document.querySelectorAll('.todo-input');
+    static download(inputType) {
+        const inputs = document.querySelectorAll(inputType);
         const data = [];
         inputs.forEach(input => data.push(input.value));
         return data;
